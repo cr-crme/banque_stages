62fdbfe44587a46c855fca1aa29e97b170cc85b2
Laurent Stéphenne <guibi.ss999@gmail.com>
2022-07-13 22:38:27 -0400

    Now using getter

--- lib/common/providers/activity_types_provider.dart
+++ lib/common/providers/activity_types_provider.dart
@@ -4,12 +4,14 @@ import '/common/models/activity_types.dart';
 
 class ActivityTypesProvider extends ChangeNotifier {
   ActivityTypesProvider({Map<ActivityTypes, bool>? activityTypes})
-      : activityTypes = activityTypes ?? ActivityTypes.emptyMap;
+      : _activityTypes = activityTypes ?? ActivityTypes.emptyMap;
 
-  final Map<ActivityTypes, bool> activityTypes;
+  final Map<ActivityTypes, bool> _activityTypes;
+
+  Map<ActivityTypes, bool> get activityTypes => _activityTypes;
 
   void update(ActivityTypes key, bool newValue) {
-    activityTypes[key] = newValue;
+    _activityTypes[key] = newValue;
     notifyListeners();
   }
 }
