a2b243c791c6c4c64659bd72eb42ea69886a3d14
Laurent Stéphenne <guibi.ss999@gmail.com>
2022-07-20 23:07:19 -0400

    Added other Job data

--- lib/common/models/job.dart
+++ lib/common/models/job.dart
@@ -1,25 +1,82 @@
 import '/misc/custom_containers/item_serializable.dart';
 
 class Job extends ItemSerializable {
-  Job({JobActivitySector? activitySector, JobSpecialization? specialization})
+  Job(
+      {JobActivitySector? activitySector,
+      JobSpecialization? specialization,
+      this.principalTask = "",
+      this.dangerousSituations = "",
+      this.protectionEquipements = "",
+      this.accidents = "",
+      this.stressSituations = "",
+      this.minimumAge = 0,
+      this.uniform = "",
+      this.expectations = "",
+      this.supervision = "",
+      this.comments = ""})
       : activitySector = activitySector ?? JobActivitySector.values.first,
         specialization = specialization ?? JobSpecialization.values.first;
 
   Job copyWith(
-      {JobActivitySector? activitySector, JobSpecialization? specialization}) {
+      {JobActivitySector? activitySector,
+      JobSpecialization? specialization,
+      String? principalTask,
+      String? dangerousSituations,
+      String? protectionEquipements,
+      String? accidents,
+      String? stressSituations,
+      int? minimumAge,
+      String? uniform,
+      String? expectations,
+      String? supervision,
+      String? comments}) {
     return Job(
         activitySector: activitySector ?? this.activitySector,
-        specialization: specialization ?? this.specialization);
+        specialization: specialization ?? this.specialization,
+        principalTask: principalTask ?? this.principalTask,
+        dangerousSituations: dangerousSituations ?? this.dangerousSituations,
+        protectionEquipements:
+            protectionEquipements ?? this.protectionEquipements,
+        accidents: accidents ?? this.accidents,
+        stressSituations: stressSituations ?? this.stressSituations,
+        minimumAge: minimumAge ?? this.minimumAge,
+        uniform: uniform ?? this.uniform,
+        expectations: expectations ?? this.expectations,
+        supervision: supervision ?? this.supervision,
+        comments: comments ?? this.comments);
   }
 
   @override
   Map<String, dynamic> serializedMap() {
-    return {"activitySector": activitySector, "specialization": specialization};
+    return {
+      "activitySector": activitySector,
+      "specialization": specialization,
+      "principalTask": principalTask,
+      "dangerousSituations": dangerousSituations,
+      "protectionEquipements": protectionEquipements,
+      "accidents": accidents,
+      "stressSituations": stressSituations,
+      "minimumAge": minimumAge,
+      "uniform": uniform,
+      "expectations": expectations,
+      "supervision": supervision,
+      "comments": comments
+    };
   }
 
   Job.fromSerialized(Map<String, dynamic> map)
       : activitySector = map['activitySector'],
         specialization = map['specialization'],
+        principalTask = map['principalTask'],
+        dangerousSituations = map['dangerousSituations'],
+        protectionEquipements = map['protectionEquipements'],
+        accidents = map['accidents'],
+        stressSituations = map['stressSituations'],
+        minimumAge = map['minimumAge'],
+        uniform = map['uniform'],
+        expectations = map['expectations'],
+        supervision = map['supervision'],
+        comments = map['comments'],
         super.fromSerialized(map);
 
   @override
@@ -29,6 +86,19 @@ class Job extends ItemSerializable {
 
   final JobActivitySector activitySector;
   final JobSpecialization specialization;
+
+  final String principalTask;
+
+  final String dangerousSituations;
+  final String protectionEquipements;
+  final String accidents;
+  final String stressSituations;
+
+  final int minimumAge;
+  final String uniform;
+  final String expectations;
+  final String supervision;
+  final String comments;
 }
 
 enum JobActivitySector {
