d118e9b44c13dcd327fe1a54078f3d54e8795e55
Guibi1 <guibi.ss999@gmail.com>
2022-08-10 16:44:11 -0400

    Updated Icons

--- lib/screens/enterprise/enterprise_screen.dart
+++ lib/screens/enterprise/enterprise_screen.dart
@@ -86,13 +86,10 @@ class _EnterpriseScreenState extends State<EnterpriseScreen>
           bottom: TabBar(
             controller: _tabController,
             tabs: const [
-              Tab(icon: Icon(Icons.info_outline), text: "À propos"),
+              Tab(icon: Icon(Icons.info_outlined), text: "À propos"),
               Tab(icon: Icon(Icons.person), text: "Contact"),
-              Tab(icon: Icon(Icons.location_city_rounded), text: "Métiers"),
-              Tab(
-                icon: Icon(Icons.notes_rounded),
-                text: "Stages",
-              ),
+              Tab(icon: Icon(Icons.handyman), text: "Métiers"),
+              Tab(icon: Icon(Icons.assignment), text: "Stages"),
             ],
           ),
         ),
