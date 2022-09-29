1087ec0671ed1d7e263577f22113ab2c8c96721e
pilotemaryse <1262635@bdeb.qc.ca>
2022-09-29 10:46:01 -0400

    First iteration of homepage SST

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:crcrme_banque_stages/crcrme_material_theme/lib/crcrme_material_theme.dart';
+import 'package:crcrme_banque_stages/screens/ref_sst/home_sst/home_sst_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
@@ -56,6 +57,7 @@ class MyApp extends StatelessWidget {
               const EnterprisesListScreen(),
           AddEnterpriseScreen.route: (context) => const AddEnterpriseScreen(),
           EnterpriseScreen.route: (context) => const EnterpriseScreen(),
+          HomeSSTScreen.route: (context) => const HomeSSTScreen(),
           PostInternshipEvaluationScreen.route: (context) =>
               const PostInternshipEvaluationScreen(),
         },
---
+++ lib/screens/ref_sst/home_sst/home_sst_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '/common/models/enterprise.dart';
+import '/common/providers/enterprises_provider.dart';
+import '/dummy_data.dart';
+import '/screens/add_enterprise/add_enterprise_screen.dart';
+import '/screens/enterprise/enterprise_screen.dart';
+import 'widgets/list_item.dart';
+import 'widgets/search_bar.dart';
+
+final ButtonStyle raisedButtonStyle = ElevatedButton.styleFrom(
+  onPrimary: Colors.black87,
+  primary: Colors.grey[300],
+  padding: EdgeInsets.symmetric(horizontal: 16),
+  shape: const RoundedRectangleBorder(
+    borderRadius: BorderRadius.all(Radius.circular(2)),
+  ),
+);
+
+class HomeSSTScreen extends StatefulWidget {
+  const HomeSSTScreen({Key? key}) : super(key: key);
+
+  static const route = "/home-sst";
+
+  @override
+  State<HomeSSTScreen> createState() => _HomeSSTScreenState();
+}
+
+class _HomeSSTScreenState extends State<HomeSSTScreen> {
+  bool _hideNotAvailable = false;
+
+  final _searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(() => setState(() {}));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Référentiel SST"),
+          actions: [
+            IconButton(
+              onPressed: () =>
+                  Navigator.pushNamed(context, AddEnterpriseScreen.route),
+              tooltip: "Ajouter une entreprise",
+              icon: const Icon(Icons.add),
+            ),
+          ],
+          bottom: SearchBar(controller: _searchController),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            //Test button
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  print('Hello');
+                },
+                child: const Text(
+                  'Consulter les fiches de risques',
+                  //textAlign: TextAlign.center,
+                  // overflow: TextOverflow.visible,
+                  //style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            //Button connected to fiches de risques
+            Center(
+                child: ElevatedButton(
+              child: const Text('Bonjour!'),
+              onPressed: () {
+                print('Hello');
+              },
+            )),
+          ],
+        ));
+  }
+}
