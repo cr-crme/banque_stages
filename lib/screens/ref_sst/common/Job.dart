7ac93d3179791e3d027d40a06968c4f06e9b3492
Sam SEBILLE <sebille.sam@gmail.com>
2022-10-03 09:33:30 -0400

    fix import job class

--- lib/screens/ref_sst/common/Job.dart
+++ lib/screens/ref_sst/common/Job.dart
@@ -1,7 +1,9 @@
+import './Skill.dart';
+
 class Job {
   String _name;
   int _code;
-  List<skills> _skills_list;
+  List<Skill> _skills_list;
 
   Job(this._name, this._code, this._skills_list);
 }
