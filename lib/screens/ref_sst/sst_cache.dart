3d1cdf933705b64298a3772830f3519cdbc24bfb
QuentinGast <2061694@bdeb.qc.ca>
2022-10-16 19:05:40 -0400

    Add method for cache singleton

--- lib/screens/ref_sst/sst_cache.dart
+++ lib/screens/ref_sst/sst_cache.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/widgets.dart';
+
 import 'common/job_sst.dart';
 import 'common/risk_sst.dart';
 import 'common/skill_sst.dart';
 import 'common/proxy_ref_sst.dart';
+import 'sst_cards/widgets/sst_card.dart';
 
 class Singleton {
   static final Singleton _singleton = Singleton._internal();
@@ -14,4 +17,23 @@ class Singleton {
   }
 
   Singleton._internal();
+
+  List<JobSST> getJobs() {
+    return jobs;
+  }
+
+  List<RiskSST> getRisks() {
+    return risks;
+  }
+
+  ListView getListViewJob() {
+    return ListView(
+        children: [for (JobSST job in jobs) SSTCard(job.jobCode, job.jobName)]);
+  }
+
+  ListView getListViewRisk() {
+    return ListView(children: [
+      for (RiskSST risk in risks) SSTCard(risk.cardID, risk.title)
+    ]);
+  }
 }
