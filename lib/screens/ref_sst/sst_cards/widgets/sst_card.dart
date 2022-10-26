a1f340ba02d3a70934f271b55ea349e772a4a947
pilotemaryse <1262635@bdeb.qc.ca>
2022-10-25 20:10:21 -0400

    added navigation to RisksCardsScreen

--- lib/screens/ref_sst/sst_cards/widgets/sst_card.dart
+++ lib/screens/ref_sst/sst_cards/widgets/sst_card.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 
+import 'package:crcrme_banque_stages/screens/ref_sst/risks_cards/risks_cards_screen.dart';
+
 //SST
 class SSTCard extends StatelessWidget {
   //params and variables
@@ -12,20 +14,22 @@ class SSTCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 0.5,
-      child: ListTile(
-        title: Text(
-          'FICHE$nmb',
-          style: Theme.of(context).textTheme.titleLarge,
-        ),
-        subtitle: Text(
-          title,
-          style: Theme.of(context).textTheme.titleMedium,
-        ),
-        isThreeLine: true,
-        //onTap should redirect to the risk
-        onTap: () => debugPrint("Clicked"),
-      ),
-    );
+        elevation: 0.5,
+        child: ListTile(
+            title: Text(
+              'FICHE$nmb',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            subtitle: Text(
+              title,
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            isThreeLine: true,
+            //onTap should redirect to the risk
+            onTap: () => Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => const RisksCardsScreen(),
+                  ),
+                )));
   }
 }
