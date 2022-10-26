97faef4c68b90d51a2943e9122c0b09fafd50429
pilotemaryse <1262635@bdeb.qc.ca>
2022-10-26 12:50:53 -0400

    center subTitle

--- lib/screens/ref_sst/risks_cards/widgets/subTitle.dart
+++ lib/screens/ref_sst/risks_cards/widgets/subTitle.dart
@@ -8,21 +8,29 @@ class SubTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-        leading: CircleAvatar(
-          backgroundColor: const Color.fromARGB(255, 190, 77, 81),
-          child: Text(
-            index.toString(),
-            style: const TextStyle(
-                color: Color.fromARGB(255, 255, 255, 255),
-                fontWeight: FontWeight.bold),
-          ),
-        ),
-        title: Text(
-          title,
-          style: TextStyle(
-              color: const Color.fromARGB(255, 1, 1, 1),
-              fontWeight: FontWeight.bold),
-        ));
+    return Column(
+      children: [
+        Center(
+          child: Container(
+              margin: const EdgeInsets.only(top: 30, right: 25, left: 25),
+              child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: const Color.fromARGB(255, 190, 77, 81),
+                    child: Text(
+                      index.toString(),
+                      style: const TextStyle(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  title: Text(
+                    title,
+                    style: const TextStyle(
+                        color: Color.fromARGB(255, 1, 1, 1),
+                        fontWeight: FontWeight.bold),
+                  ))),
+        )
+      ],
+    );
   }
 }
