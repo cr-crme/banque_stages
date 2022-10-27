cc612fd7e950784a8580ceab68ab7b5e845ac45c
pilotemaryse <1262635@bdeb.qc.ca>
2022-10-26 22:02:44 -0400

    method to build a subpoint in a paragraph

--- lib/screens/ref_sst/risks_cards/widgets/paragraph.dart
+++ lib/screens/ref_sst/risks_cards/widgets/paragraph.dart
@@ -3,22 +3,34 @@ import 'package:flutter/material.dart';
 class Paragraph extends StatelessWidget {
   //params and variables
   const Paragraph(this.texts, {super.key});
-  final List<String> texts;
+  final Map<String, List<String>> texts;
   @override
   Widget build(BuildContext context) {
     var widgetList = <Widget>[];
-    for (var text in texts) {
-      widgetList.add(ConstructParagraph(text));
+    for (MapEntry<String, dynamic> point in texts.entries) {
+      widgetList.add(ConstructLineWithDot(point.key));
+      for (String subPoint in point.value) {
+        widgetList.add(ConstructSubPoint(subPoint));
+      }
       widgetList.add(const SizedBox(
         height: 5.0,
       ));
     }
-    return Column(children: widgetList);
+    return Center(
+      child: Container(
+        margin: const EdgeInsets.only(
+          top: 30,
+          right: 25,
+          left: 25,
+        ),
+        child: Column(children: widgetList),
+      ),
+    );
   }
 }
 
-class ConstructParagraph extends StatelessWidget {
-  const ConstructParagraph(this.text, {super.key});
+class ConstructLineWithDot extends StatelessWidget {
+  const ConstructLineWithDot(this.text, {super.key});
   final String text;
 
   @override
@@ -27,6 +39,24 @@ class ConstructParagraph extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         const Text("• "),
+        Expanded(
+          child: Text(text),
+        ),
+      ],
+    );
+  }
+}
+
+class ConstructSubPoint extends StatelessWidget {
+  const ConstructSubPoint(this.text, {super.key});
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const Text("      ◦ "),
         Expanded(
           child: Text(text),
         ),
