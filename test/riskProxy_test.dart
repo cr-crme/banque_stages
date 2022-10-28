67d956a6ce17f1e4d002a37c3eac0f11042ef340
Sam SEBILLE <sebille.sam@gmail.com>
2022-10-28 16:27:42 -0400

    restruture tests

--- test/riskProxy_test.dart
+++ test/riskProxy_test.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:crcrme_banque_stages/screens/ref_sst/common/card_sst.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
@@ -10,11 +13,36 @@ import 'riskProxy_test.mocks.dart';
 
 @GenerateMocks([cardsProxy])
 void main() {
+
+  Map<String,List<String>> filledMap(String name,int items) {
+
+    Map<String,List<String>> list_map = {};
+
+    for (int i = 0; i <items; i++) {
+      list_map["$name $i"] = List<String>.filled(3, "lorem ipsum");
+    }
+
+    return list_map;
+  }
+
   List<CardSST> createDummyData() {
+
     List<CardSST> list_dummy = <CardSST>[];
+    List<RiskSST> list_risk = <RiskSST>[];
+    List<LinkSST> list_link = <LinkSST>[];
+
+    list_link = List<LinkSST>.filled(3, new LinkSST(source: "Source", title: "Title", url: "url"));
+
+    Map<String,List<String>> list_situation = filledMap("Situation", 4);
+    Map<String,List<String>> list_factor = filledMap("Factor", 3);
+    Map<String,List<String>> list_symptoms = filledMap("Symptom", 4);
+
     for (int i = 0; i < 10; i++) {
-      list_dummy
-          .add(new CardSST(id: i, shortname: "num $i", title: "TITLE $i"));
+      list_risk.add(new RiskSST(id: i, title: "title $i", intro: "intro $i", situations: list_situation, factors: list_factor , symptoms: list_symptoms, images: List<String>.filled(2, "images")));
+    }
+
+    for (int i = 0; i < 10; i++) {
+      list_dummy.add(new CardSST(id: i, shortname: "num $i", name: "TITLE $i",links: list_link,risks: list_risk ));
     }
     return list_dummy;
   }
@@ -39,6 +67,23 @@ void main() {
     test('Test get list risks : number object list', () {
       when(riskProxy.getList()).thenReturn(dummy_list_risk);
       expect(riskProxy.getList().length, 10);
+    });
+
+    test('Test non-null values', (){
+      when(riskProxy.getList()).thenReturn(dummy_list_risk);
+      String error = "";
+      if(dummy_list_risk[0] == null){
+        error += "Id is null\n";
+      }if(dummy_list_risk[1] == null){
+        error += "Shortname is null\n";
+      }if(dummy_list_risk[2] == null){
+        error += "Name is null\n";
+      }if(dummy_list_risk[4] == null){
+        error += "Links is null\n";
+      }if(dummy_list_risk[5] == null){
+        error += "Risks is null\n";
+      }
+      expect("", error);
     });
   });
 }
