814bf5a13164dffd943e721a104e60797aa12731
pilotemaryse <1262635@bdeb.qc.ca>
2022-10-29 14:45:00 -0400

    Create factors.dart

---
+++ lib/screens/ref_sst/risks_cards/widgets/factors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:crcrme_banque_stages/screens/ref_sst/risks_cards/widgets/paragraph.dart';
+
+class Factors extends StatelessWidget {
+  //params and variables
+  const Factors(this.texts, {super.key});
+  final Map<String, List<String>> texts;
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Container(
+        margin: const EdgeInsets.only(top: 30, right: 25, left: 10),
+        child: ListTile(
+          textColor: Colors.black,
+          title: const Padding(
+            padding: EdgeInsets.only(bottom: 5),
+            child: Text(
+              "PRINCIPAUX FACTEURS AGGRAVANTS",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 190, 77, 81)),
+            ),
+          ),
+          subtitle: Paragraph(texts),
+        ),
+      )
+    ]);
+  }
+}
