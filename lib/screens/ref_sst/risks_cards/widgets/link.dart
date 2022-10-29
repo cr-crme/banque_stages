4acaedee658a463563ee5b6a8ae0f7398d9aa64c
pilotemaryse <1262635@bdeb.qc.ca>
2022-10-29 15:37:59 -0400

    refactor name of a package

--- lib/screens/ref_sst/risks_cards/widgets/link.dart
+++ lib/screens/ref_sst/risks_cards/widgets/link.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:crcrme_banque_stages/screens/ref_sst/common/card_sst.dart';
-import 'package:crcrme_banque_stages/screens/ref_sst/risks_cards/widgets/listLinks.dart';
+import 'package:crcrme_banque_stages/screens/ref_sst/risks_cards/widgets/list_links.dart';
 
 class Link extends StatelessWidget {
   //params and variables
