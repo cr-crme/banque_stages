77838e796b71df0f56079e57b0228575afe4cea3
Pariterre <pariterre@hotmail.com>
2022-11-02 15:49:14 -0400

    Simplified CardEnterprise and made text Flexible

--- lib/screens/enterprises_list/widgets/enterprise_card.dart
+++ lib/screens/enterprises_list/widgets/enterprise_card.dart
@@ -17,73 +17,53 @@ class EnterpriseCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       elevation: 0.5,
-      child: InkWell(
+      child: ListTile(
         onTap: () => onTap(enterprise),
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Row(
+        leading: Container(
+          width: 80,
+          height: 60,
+          color: Theme.of(context).disabledColor,
+          child: enterprise.photo.isNotEmpty
+              ? Image.network(enterprise.photo)
+              : null,
+        ),
+        title: Text(
+          enterprise.name,
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        subtitle: Visibility(
+          visible: enterprise.address.isNotEmpty ||
+              enterprise.headquartersAddress.isNotEmpty,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                width: 80,
-                height: 60,
-                color: Theme.of(context).disabledColor,
-                child: enterprise.photo.isNotEmpty
-                    ? Image.network(enterprise.photo)
-                    : null,
+              const SizedBox(height: 8),
+              Text(
+                enterprise.address.isEmpty
+                    ? enterprise.headquartersAddress
+                    : enterprise.address,
               ),
-              const SizedBox(width: 16),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      enterprise.name,
-                      style: Theme.of(context).textTheme.titleMedium,
-                    ),
-                    Visibility(
-                      visible: enterprise.address.isNotEmpty ||
-                          enterprise.headquartersAddress.isNotEmpty,
-                      child: Column(
-                        children: [
-                          const SizedBox(height: 8),
-                          Text(
-                            enterprise.address.isEmpty
-                                ? enterprise.headquartersAddress
-                                : enterprise.address,
+              ...enterprise.jobs
+                  .map((job) => Padding(
+                      padding: const EdgeInsets.only(top: 8),
+                      child: Row(children: [
+                        DisponibilityCircle(
+                            positionsOffered: job.positionsOffered,
+                            positionsOccupied: job.positionsOccupied),
+                        const SizedBox(width: 8),
+                        Flexible(
+                          child: Text(
+                            job.specialization?.idWithName ?? "bad id",
                           ),
-                        ],
-                      ),
-                    ),
-                    Column(
-                      children: enterprise.jobs
-                          .map((job) => Padding(
-                                padding: const EdgeInsets.only(top: 8),
-                                child: Row(
-                                  children: [
-                                    DisponibilityCircle(
-                                        positionsOffered: job.positionsOffered,
-                                        positionsOccupied:
-                                            job.positionsOccupied),
-                                    const SizedBox(width: 8),
-                                    Text(
-                                      job.specialization?.idWithName ??
-                                          "bad id",
-                                    ),
-                                  ],
-                                ),
-                              ))
-                          .toList(),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(width: 16),
-              Icon(
-                Icons.chevron_right,
-                color: Theme.of(context).hintColor,
-              ),
+                        ),
+                      ])))
+                  .toList()
             ],
           ),
+        ),
+        trailing: Icon(
+          Icons.chevron_right,
+          color: Theme.of(context).hintColor,
         ),
       ),
     );
