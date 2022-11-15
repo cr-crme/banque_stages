b26af5d051b244cc2262a80526128889bd925de7
Laurent Stéphenne <guibi.ss999@gmail.com>
2022-11-15 14:55:39 -0500

    Fixed Regex not scanning the entire String

--- lib/misc/form_service.dart
+++ lib/misc/form_service.dart
@@ -52,7 +52,7 @@ abstract class FormService {
       return _localizations.emptyEmailError;
     }
     if (!RegExp(
-            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+$")
         .hasMatch(email)) {
       return _localizations.invalidEmailError;
     }
