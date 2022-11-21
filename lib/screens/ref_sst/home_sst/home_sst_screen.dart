70ae9cc130ba7ea6f4a0d24f21bf8003252b5da3
Quentin Gastaldo <78386927+QuentinGast@users.noreply.github.com>
2022-11-21 09:48:33 -0500

    implementation of future builder

--- lib/screens/ref_sst/home_sst/home_sst_screen.dart
+++ lib/screens/ref_sst/home_sst/home_sst_screen.dart
@@ -36,36 +36,81 @@ class _HomeSSTScreenState extends State<HomeSSTScreen> {
     return await Future<String>.value(_ref.child("01").onValue.toString());
   }
 
+  bool test = true;
+
   @override
   Widget build(BuildContext context) {
+    Widget body = Center(child: CircularProgressIndicator());
+    ;
     var data = Provider.of<RisksProvider>(context, listen: true);
-    if (!data.hasData) {
+    if (test) {
+      //data.isEmpty
       return FutureBuilder<String>(
           future: fetchRisks(),
           builder: (ctx, snapshot) {
-            if (snapshot.hasData) data.deserialize(snapshot.data!);
-            return Scaffold(
-                appBar: AppBar(
-                  title: const Text("Référentiel SST"),
-                ),
-                drawer: const MainDrawer(),
-                body: Center(child: const CircularProgressIndicator()));
+            if (snapshot.hasData) {
+              //data.deserialize(snapshot.data!);
+              test = false;
+            }
           });
     } else {
-      return Scaffold(
-          appBar: AppBar(
-            title: const Text("Référentiel SST"),
-          ),
-          drawer: const MainDrawer(),
-          body: ListView(
-            children: [
-              //Container for the search bar
-              Center(
-                  child: Container(
-                margin: const EdgeInsets.only(top: 50.0),
-                width: 300,
-                height: 260,
-                padding: const EdgeInsets.all(17.0),
+      body = ListView(
+        children: [
+          //Container for the search bar
+          Center(
+              child: Container(
+            margin: const EdgeInsets.only(top: 50.0),
+            width: 300,
+            height: 260,
+            padding: const EdgeInsets.all(17.0),
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              color: Colors.blue,
+              boxShadow: [
+                BoxShadow(color: Colors.grey, spreadRadius: 1, blurRadius: 15)
+              ],
+            ),
+            child: InkWell(
+                onTap: () {
+                  print("Clicked on jod list risks and skills");
+                },
+                child: Column(
+                  children: [
+                    const Text("Analyse des risques par métier",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                            color: Colors.white,
+                            fontFamily: "Noto Sans")),
+                    Padding(
+                        padding: const EdgeInsets.only(top: 25.0),
+                        child: SearchBar(controller: _searchController)),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 30.0),
+                      child: Text(
+                        "L'analyse des risques à la SST a été faite pour les 45 métiers les plus populaires du répertoires du Ministère de l'éducation.",
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                            fontSize: 11,
+                            color: Colors.white,
+                            fontStyle: FontStyle.italic),
+                      ),
+                    )
+                  ],
+                )),
+          )),
+          Center(
+              child: Container(
+            margin: const EdgeInsets.only(top: 35.0),
+            child: InkWell(
+              onTap: () {
+                print("Clicked on sst cards list");
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (_) => SSTCardsScreen(),
+                ));
+              },
+              child: Ink(
                 decoration: const BoxDecoration(
                   borderRadius: BorderRadius.all(Radius.circular(10)),
                   color: Colors.blue,
@@ -74,77 +119,34 @@ class _HomeSSTScreenState extends State<HomeSSTScreen> {
                         color: Colors.grey, spreadRadius: 1, blurRadius: 15)
                   ],
                 ),
-                child: InkWell(
-                    onTap: () {
-                      print("Clicked on jod list risks and skills");
-                    },
-                    child: Column(
-                      children: [
-                        const Text("Analyse des risques par métier",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 25,
-                                color: Colors.white,
-                                fontFamily: "Noto Sans")),
-                        Padding(
-                            padding: const EdgeInsets.only(top: 25.0),
-                            child: SearchBar(controller: _searchController)),
-                        const Padding(
-                          padding: EdgeInsets.only(top: 30.0),
-                          child: Text(
-                            "L'analyse des risques à la SST a été faite pour les 45 métiers les plus populaires du répertoires du Ministère de l'éducation.",
-                            textAlign: TextAlign.justify,
-                            style: TextStyle(
-                                fontSize: 11,
-                                color: Colors.white,
-                                fontStyle: FontStyle.italic),
-                          ),
-                        )
-                      ],
-                    )),
-              )),
-              Center(
-                  child: Container(
-                margin: const EdgeInsets.only(top: 35.0),
-                child: InkWell(
-                  onTap: () {
-                    print("Clicked on sst cards list");
-                    Navigator.of(context).push(MaterialPageRoute(
-                      builder: (_) => SSTCardsScreen(),
-                    ));
-                  },
-                  child: Ink(
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      color: Colors.blue,
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.grey, spreadRadius: 1, blurRadius: 15)
-                      ],
-                    ),
-                    width: 300,
-                    height: 260,
-                    child: const Padding(
-                      padding: EdgeInsets.all(45.0),
-                      child: Center(
-                        child: Text(
-                          "Consulter les fiches de risques",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 25,
-                              color: Colors.white,
-                              fontFamily: "Noto Sans"),
-                        ),
-                      ),
+                width: 300,
+                height: 260,
+                child: const Padding(
+                  padding: EdgeInsets.all(45.0),
+                  child: Center(
+                    child: Text(
+                      "Consulter les fiches de risques",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                          color: Colors.white,
+                          fontFamily: "Noto Sans"),
                     ),
                   ),
                 ),
-              )),
-            ],
-          ));
+              ),
+            ),
+          )),
+        ],
+      );
     }
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Référentiel SST"),
+        ),
+        drawer: const MainDrawer(),
+        body: body);
   }
 
   void _activateListeners() {
