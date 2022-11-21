a33693a96065ccee3f1857688269f4ac51a24443
QuentinGast <2061694@bdeb.qc.ca>
2022-11-21 18:49:51 -0500

    quick cleanup

--- lib/screens/ref_sst/home_sst/home_sst_screen.dart
+++ lib/screens/ref_sst/home_sst/home_sst_screen.dart
@@ -1,12 +1,8 @@
 import 'package:crcrme_banque_stages/common/providers/risks_provider.dart';
-import 'package:crcrme_banque_stages/common/providers/students_provider.dart';
 import 'package:crcrme_banque_stages/screens/ref_sst/sst_cards/sst_cards_screen.dart';
-import 'package:crcrme_banque_stages/screens/ref_sst/job_list_risks_and_skills/job_list_screen.dart';
-import 'package:enhanced_containers/database_list_provided.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../common/Risk.dart';
 import 'widgets/search_bar.dart';
 import '/common/widgets/main_drawer.dart';
 
@@ -23,13 +19,10 @@ class _HomeSSTScreenState extends State<HomeSSTScreen> {
   final _searchController = TextEditingController();
   final _ref = FirebaseDatabase.instance.ref();
 
-  Future<String> json = Future<String>.value("");
-
   @override
   void initState() {
     super.initState();
     _searchController.addListener(() => setState(() {}));
-    _activateListeners();
   }
 
   Future<String> fetchRisks() async {
@@ -48,7 +41,6 @@ class _HomeSSTScreenState extends State<HomeSSTScreen> {
       body = FutureBuilder<String>(
           future: fetchRisks(),
           builder: (ctx, snapshot) {
-            //print("prout");
             if (snapshot.hasData) {
               //data.deserialize(snapshot.data!);
               var datatest = snapshot.data!;
@@ -75,41 +67,37 @@ class _HomeSSTScreenState extends State<HomeSSTScreen> {
               ],
             ),
             child: InkWell(
-                onTap: () {
-                  print("Clicked on job list risks and skills");
-                },
                 child: Column(
-                  children: [
-                    const Text("Analyse des risques par métier",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 25,
-                            color: Colors.white,
-                            fontFamily: "Noto Sans")),
-                    Padding(
-                        padding: const EdgeInsets.only(top: 25.0),
-                        child: SearchBar(controller: _searchController)),
-                    const Padding(
-                      padding: EdgeInsets.only(top: 30.0),
-                      child: Text(
-                        "L'analyse des risques à la SST a été faite pour les 45 métiers les plus populaires du répertoires du Ministère de l'éducation.",
-                        textAlign: TextAlign.justify,
-                        style: TextStyle(
-                            fontSize: 11,
-                            color: Colors.white,
-                            fontStyle: FontStyle.italic),
-                      ),
-                    )
-                  ],
-                )),
+              children: [
+                const Text("Analyse des risques par métier",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontFamily: "Noto Sans")),
+                Padding(
+                    padding: const EdgeInsets.only(top: 25.0),
+                    child: SearchBar(controller: _searchController)),
+                const Padding(
+                  padding: EdgeInsets.only(top: 30.0),
+                  child: Text(
+                    "L'analyse des risques à la SST a été faite pour les 45 métiers les plus populaires du répertoires du Ministère de l'éducation.",
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
+                        fontSize: 11,
+                        color: Colors.white,
+                        fontStyle: FontStyle.italic),
+                  ),
+                )
+              ],
+            )),
           )),
           Center(
               child: Container(
             margin: const EdgeInsets.only(top: 35.0),
             child: InkWell(
               onTap: () {
-                print("Clicked on sst cards list");
                 Navigator.of(context).push(MaterialPageRoute(
                   builder: (_) => SSTCardsScreen(),
                 ));
@@ -145,6 +133,7 @@ class _HomeSSTScreenState extends State<HomeSSTScreen> {
         ],
       );
     }
+    // To refresh the scaffold body after the data fetch
     WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
     return Scaffold(
         appBar: AppBar(
