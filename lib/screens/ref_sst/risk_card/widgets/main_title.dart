450ba31230c45547b0db8371389d964ee32017ea
pilotemaryse <1262635@bdeb.qc.ca>
2022-12-13 09:29:13 -0500

    updated style for main title risk card

--- lib/screens/ref_sst/risk_card/widgets/main_title.dart
+++ lib/screens/ref_sst/risk_card/widgets/main_title.dart
@@ -6,16 +6,15 @@ class MainTitle extends StatelessWidget {
   final String mainTitle;
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Center(
-        child: Container(
-          margin: const EdgeInsets.only(top: 30, right: 25, left: 25),
-          child: Text(
-            mainTitle,
-            style: Theme.of(context).textTheme.titleLarge,
-          ),
+    return Container(
+      child: Container(
+        margin: const EdgeInsets.only(top: 30, right: 25, left: 25, bottom: 25),
+        child: Text(
+          mainTitle,
+          style: Theme.of(context).textTheme.titleLarge,
+          textAlign: TextAlign.left,
         ),
-      )
-    ]);
+      ),
+    );
   }
 }
