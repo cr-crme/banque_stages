8b72bdd0f134954596c22ae51f025d096489d7ee
Pariterre <pariterre@hotmail.com>
2023-03-10 17:38:42 -0500

    Added the search capability in supervision charts

--- lib/screens/supervision_chart/supervision_chart_screen.dart
+++ lib/screens/supervision_chart/supervision_chart_screen.dart
@@ -1,23 +1,50 @@
 import 'package:flutter/material.dart';
 
+import '/common/models/student.dart';
+import '/common/models/visiting_priority.dart';
+import '/common/providers/students_provider.dart';
 import '/common/widgets/main_drawer.dart';
 
-class SupervisionChart extends StatelessWidget {
+class SupervisionChart extends StatefulWidget {
   const SupervisionChart({super.key});
+
+  @override
+  State<SupervisionChart> createState() => _SupervisionChartState();
+}
+
+class _SupervisionChartState extends State<SupervisionChart> {
+  bool _isSearchBarExpanded = false;
+  final _searchTextController = TextEditingController();
+
+  void _toggleSearchBar() {
+    _isSearchBarExpanded = !_isSearchBarExpanded;
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
     final iconSize = screenSize.width / 16;
 
+    // TODO: Get student of the current teacher
+    final studentsTp = StudentsProvider.of(context);
+    List<Student> students = studentsTp
+        .map<Student?>((e) => _searchTextController.text == ''
+            ? e
+          : e.name
+                    .toLowerCase()
+                    .contains(_searchTextController.text.toLowerCase())
+                ? e
+                : null)
+        .where((e) => e != null)
+        .toList()
+        .cast<Student>();
+
     return Scaffold(
       appBar: AppBar(
-        title: const Padding(
-          padding: EdgeInsets.only(top: 5.0, bottom: 5),
-          child: Text('Élèves à superviser'),
-        ),
+        title: const Text('Élèves à superviser'),
         bottom: PreferredSize(
-            preferredSize: Size(screenSize.width, iconSize),
+            preferredSize: Size(screenSize.width, iconSize * 1.5),
             child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
@@ -29,7 +56,7 @@ class SupervisionChart extends StatelessWidget {
                   _TabIcon(
                       screenSize: screenSize,
                       iconSize: iconSize,
-                      onTap: () {},
+                      onTap: _toggleSearchBar,
                       icon: Icons.search),
                   _TabIcon(
                       screenSize: screenSize,
@@ -38,7 +65,46 @@ class SupervisionChart extends StatelessWidget {
                       icon: Icons.filter_alt_sharp),
                 ])),
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            if (_isSearchBarExpanded)
+              Container(
+                margin: const EdgeInsets.all(8),
+                padding: const EdgeInsets.only(left: 15, right: 15),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.search),
+                      labelText: 'Rechercher un métier',
+                      suffixIcon: IconButton(
+                          onPressed: () => _searchTextController.text = '',
+                          icon: const Icon(Icons.clear)),
+                      border:
+                          const OutlineInputBorder(borderSide: BorderSide())),
+                  controller: _searchTextController,
+                  onChanged: (value) => setState(() {}),
+                ),
+              ),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: students.length,
+              itemBuilder: ((ctx, i) {
+                final internship = students[i].internships.isNotEmpty
+                    ? students[i].internships.last
+                    : null;
+                return _StudentTile(
+                  key: Key(students[i].id),
+                  name: students[i].name,
+                  job: internship ?? 'Aucun stage', // TODO verify that
+                  business: internship ?? 'Aucun stage', // TODO verify that
+                  priority: VisitingPriority.low,
+                  avatar: const CircleAvatar(),
+                );
+              }),
+            ),
+          ],
+        ),
+      ),
       drawer: const MainDrawer(),
     );
   }
@@ -68,6 +134,67 @@ class _TabIcon extends StatelessWidget {
         child: Icon(
           icon,
           size: iconSize,
+        ),
+      ),
+    );
+  }
+}
+
+class _StudentTile extends StatelessWidget {
+  const _StudentTile({
+    super.key,
+    required this.name,
+    required this.business,
+    required this.avatar,
+    required this.job,
+    required this.priority,
+  });
+
+  final String name;
+  final String business;
+  final Widget avatar;
+  final String job;
+  final VisitingPriority priority;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      child: ListTile(
+        leading: SizedBox(
+          height: double.infinity, // This centers the avatar
+          child: avatar,
+        ),
+        title: Text(name),
+        isThreeLine: true,
+        subtitle: Text(
+          '$business\n$job',
+          maxLines: 2,
+          style: const TextStyle(color: Colors.black87),
+        ),
+        trailing: Ink(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 5.0,
+                spreadRadius: 0.0,
+                offset: Offset(2.0, 2.0),
+              )
+            ],
+            border: Border.all(color: Colors.lightBlue, width: 3),
+            shape: BoxShape.circle,
+          ),
+          child: IconButton(
+            onPressed: () {},
+            alignment: Alignment.center,
+            icon: Icon(
+              priority.icon,
+              color: priority.color,
+              size: 30,
+            ),
+          ),
         ),
       ),
     );
