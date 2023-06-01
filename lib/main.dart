2fe5e061e6ae2e2c918df441125ab52a158db95f
atcrme <aurelie.tondoux.hsj@ssss.gouv.qc.ca>
2023-06-01 17:20:02 -0400

    Set emulator false

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:url_strategy/url_strategy.dart';
 
-bool useEmulator = true;
+bool useEmulator = false;
 bool populateWithDebugData = kDebugMode;
 
 void main() async {
