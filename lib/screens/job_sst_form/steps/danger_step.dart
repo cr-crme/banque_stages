45d993c6d1b22a568c7d085e2dbf660a809c60f2
Pariterre <pariterre@hotmail.com>
2023-07-31 13:15:56 -0400

    Temporarily removed boxes

--- lib/screens/job_sst_form/steps/danger_step.dart
+++ lib/screens/job_sst_form/steps/danger_step.dart
@@ -1,7 +1,5 @@
 import 'package:crcrme_banque_stages/common/models/job.dart';
-import 'package:crcrme_banque_stages/common/widgets/form_fields/question_with_checkbox_list.dart';
-import 'package:crcrme_banque_stages/common/widgets/form_fields/question_with_radio_bool.dart';
-import 'package:crcrme_banque_stages/common/widgets/form_fields/question_with_text.dart';
+import 'package:crcrme_banque_stages/common/widgets/form_fields/text_with_form.dart';
 import 'package:flutter/material.dart';
 
 class DangerStep extends StatefulWidget {
@@ -35,44 +33,44 @@ class DangerStepState extends State<DangerStep> {
       child: SingleChildScrollView(
         child: Column(
           children: [
-            QuestionWithText(
+            TextWithForm(
               initialValue:
                   listToString(widget.job.sstEvaluation.dangerousSituations),
-              question:
+              title:
                   '1. Quelles sont les situations de travail qui pourraient être '
                   'dangereuses pour mon élève? Comment faudrait-il l\'y préparer?',
               onSaved: (text) => dangerousSituations = text ?? '',
             ),
-            QuestionWithCheckboxList(
-              initialChoices:
-                  widget.job.sstEvaluation.equipmentRequired.toSet(),
-              choicesQuestion:
-                  '2. Est-ce que l\'élève devra porter un des équipements de '
-                  'protection individuelle suivants?',
-              choices: const {
-                'Chaussures à semelles antidérapantes',
-                'Chaussures de sécurité',
-                'Lunettes de sécurité',
-                'Protections auditives (p. ex. bouchons)',
-                'Masque',
-                'Casque',
-                'Gants',
-              },
-              onSavedChoices: (choices) =>
-                  equipmentRequired = choices!.toList(),
-            ),
-            QuestionWithRadioBool(
-              initialChoice: widget.job.sstEvaluation.incidents.isNotEmpty,
-              initialText: listToString(widget.job.sstEvaluation.incidents),
-              choiceQuestion:
-                  '3. Est-ce qu\'il y a déjà eu des incidents ou des accidents du '
-                  'travail au poste que l\'élève occupera en stage?',
-              textQuestion: 'Pouvez-vous me raconter ce qu\'il s\'est passé?',
-              onSavedText: (text) => pastIncidents = text!,
-            ),
-            QuestionWithText(
+            // QuestionWithCheckboxList(
+            //   initialChoices:
+            //       widget.job.sstEvaluation.equipmentRequired.toSet(),
+            //   choicesQuestion:
+            //       '2. Est-ce que l\'élève devra porter un des équipements de '
+            //       'protection individuelle suivants?',
+            //   choices: const {
+            //     'Chaussures à semelles antidérapantes',
+            //     'Chaussures de sécurité',
+            //     'Lunettes de sécurité',
+            //     'Protections auditives (p. ex. bouchons)',
+            //     'Masque',
+            //     'Casque',
+            //     'Gants',
+            //   },
+            //   onSavedChoices: (choices) =>
+            //       equipmentRequired = choices!.toList(),
+            // ),
+            // QuestionWithRadioBool(
+            //   initialChoice: widget.job.sstEvaluation.incidents.isNotEmpty,
+            //   initialText: listToString(widget.job.sstEvaluation.incidents),
+            //   choiceQuestion:
+            //       '3. Est-ce qu\'il y a déjà eu des incidents ou des accidents du '
+            //       'travail au poste que l\'élève occupera en stage?',
+            //   textQuestion: 'Pouvez-vous me raconter ce qu\'il s\'est passé?',
+            //   onSavedText: (text) => pastIncidents = text!,
+            // ),
+            TextWithForm(
               initialValue: widget.job.sstEvaluation.incidentContact,
-              question:
+              title:
                   '4. À quelle personne dans l\'entreprise, l\'élève doit-il '
                   's\'adresser en cas de blessure ou d\'incident?',
               onSaved: (text) => incidentContact = text!,
