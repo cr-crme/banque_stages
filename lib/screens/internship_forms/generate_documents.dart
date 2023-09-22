e52384a268002688ebdcc89bf832ffda840b9d3a
raerae02 <95396446+raerae02@users.noreply.github.com>
2023-09-22 13:58:50 -0400

    A way to generate pdfs from json
    - created a generatePdfFromJson that takes a json as a parameter and returns a pdf document.
    - modified the generateCnesstPdf so that it implements the new method.
    - corrected a typo in generateInternshipContractPdf

--- lib/screens/internship_forms/generate_documents.dart
+++ lib/screens/internship_forms/generate_documents.dart
@@ -1,12 +1,95 @@
-import 'dart:typed_data';
-
+import 'dart:convert';
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:pdf/widgets.dart' as pw;
 
 import 'package:crcrme_banque_stages/common/models/internship.dart';
 
 class GenerateDocuments {
-  static Future<Uint8List> generateIntershipContractPdf(format,
+  Future<pw.Document> generatePdfFromJson(String jsonString) async {
+    var jsonData = json.decode(jsonString);
+    pw.Document pdf = pw.Document();
+
+    for (var page in jsonData['pages']) {
+      pdf.addPage(
+        pw.Page(
+          build: (pw.Context context) {
+            List<pw.Widget> widgets = [];
+
+            for (var element in page['elements']) {
+              if (element['type'] == 'Container') {
+                widgets.add(_createContainer(element));
+              } else if (element['type'] == 'SizedBox') {
+                widgets.add(pw.SizedBox(height: element['height'].toDouble()));
+              } else if (element['type'] == 'Text') {
+                widgets.add(_createText(element));
+              }
+            }
+            return pw.Column(
+                crossAxisAlignment: pw.CrossAxisAlignment.start,
+                children: widgets);
+          },
+        ),
+      );
+    }
+    return pdf;
+  }
+
+  pw.Container _createContainer(element) {
+    List<pw.Widget> children = [];
+    for (var child in element['children']) {
+      if (child['type'] == 'Text') {
+        children.add(_createText(child));
+      }
+    }
+    return pw.Container(
+      alignment: _getAlignment(element['alignment']),
+      padding: pw.EdgeInsets.fromLTRB(
+          element['padding'][0].toDouble(),
+          element['padding'][1].toDouble(),
+          element['padding'][2].toDouble(),
+          element['padding'][3].toDouble()),
+      decoration: _getDecoration(element['decoration']),
+      child: pw.Column(
+          crossAxisAlignment: pw.CrossAxisAlignment.start, children: children),
+    );
+  }
+
+  pw.Text _createText(Map<String, dynamic> element) {
+    return pw.Text(element['text'], style: _getTextStyle(element['style']));
+  }
+
+  pw.TextStyle _getTextStyle(Map<String, dynamic>? style) {
+    if (style == null) return const pw.TextStyle();
+    return pw.TextStyle(
+      fontWeight: style['fontWeight'] == 'bold'
+          ? pw.FontWeight.bold
+          : pw.FontWeight.normal,
+    );
+  }
+
+  pw.Alignment _getAlignment(alignment) {
+    switch (alignment) {
+      case 'topLeft':
+        return pw.Alignment.topLeft;
+      default:
+        return pw.Alignment.topLeft;
+    }
+  }
+
+  pw.BoxDecoration _getDecoration(decoration) {
+    switch (decoration) {
+      case 'border':
+        return pw.BoxDecoration(border: pw.Border.all());
+      case 'bottomBorder':
+        return const pw.BoxDecoration(
+            border: pw.Border(bottom: pw.BorderSide()));
+      default:
+        return const pw.BoxDecoration();
+    }
+  }
+
+  static Future<Uint8List> generateInternshipContractPdf(format,
       {required Internship internship, required int versionIndex}) async {
     final document = pw.Document();
 
@@ -38,14 +121,10 @@ class GenerateDocuments {
 
   static Future<Uint8List> generateCnesstPdf(format,
       {required Internship internship}) async {
-    final document = pw.Document();
+    String jsonString =
+        await rootBundle.loadString('assets/documents/cnesst.json');
 
-    document.addPage(
-      pw.Page(
-        build: (pw.Context context) => pw.Center(child: pw.Text('CNESST')),
-      ),
-    );
-
+    final document = await GenerateDocuments().generatePdfFromJson(jsonString);
     return document.save();
   }
 
