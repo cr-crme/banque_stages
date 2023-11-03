b573910b1b67edb648c2d9ffdd76d9da113db7d6
Pariterre <pariterre@hotmail.com>
2023-11-03 10:31:28 -0400

    Standardized and tested date picker

--- test/common_tests/widgets_unit_tests/utils.dart
+++ test/common_tests/widgets_unit_tests/utils.dart
@@ -1,5 +1,6 @@
 import 'package:crcrme_material_theme/crcrme_material_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 ///
 /// [loadTheme] will use the CR-CRME theme. This however connects to GoogleFonts
@@ -10,5 +11,11 @@ Widget declareWidget(Widget child, {bool loadTheme = false}) {
       body: child,
     ),
     theme: loadTheme ? crcrmeMaterialTheme : null,
+    localizationsDelegates: const [
+      GlobalMaterialLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+      GlobalCupertinoLocalizations.delegate,
+    ],
+    supportedLocales: const [Locale('fr', 'CA')],
   );
 }
