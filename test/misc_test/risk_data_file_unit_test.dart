fe412619c7eaef5baef14d18652cdef054c7996c
Pariterre <pariterre@hotmail.com>
2023-11-10 11:32:28 -0500

    Added RiskDataFile tests

---
+++ test/misc_test/risk_data_file_unit_test.dart
@@ -0,0 +1,35 @@
+import 'package:crcrme_banque_stages/initialize_program.dart';
+import 'package:crcrme_banque_stages/misc/risk_data_file_service.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('RiskDataFileService', () {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    initializeProgram(useDatabaseEmulator: true, mockFirebase: true);
+
+    test('risks are loaded properly', () async {
+      await RiskDataFileService.loadData();
+      expect(RiskDataFileService.risks, isNotEmpty);
+    });
+
+    test('can get a risk from id', () async {
+      await RiskDataFileService.loadData();
+
+      final risk = RiskDataFileService.risks[0];
+      expect(RiskDataFileService.fromId(risk.id), risk);
+
+      // Is null if not found
+      expect(RiskDataFileService.fromId('not found'), isNull);
+    });
+
+    test('can get a risk from abbrv', () async {
+      await RiskDataFileService.loadData();
+
+      final risk = RiskDataFileService.risks[0];
+      expect(RiskDataFileService.fromAbbrv(risk.abbrv), risk);
+
+      // Is null if not found
+      expect(RiskDataFileService.fromAbbrv('not found'), isNull);
+    });
+  });
+}
