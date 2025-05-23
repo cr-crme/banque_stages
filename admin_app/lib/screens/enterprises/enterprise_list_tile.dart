02d46c357b7d1dbedd5c2a29a6e9c37fdccfa504
Pariterre <pariterre@hotmail.com>
2025-05-23 09:42:45 -0400

    Added contact to enterprise list tile

--- admin_app/lib/screens/enterprises/enterprise_list_tile.dart
+++ admin_app/lib/screens/enterprises/enterprise_list_tile.dart
@@ -2,7 +2,10 @@ import 'package:admin_app/providers/enterprises_provider.dart';
 import 'package:admin_app/screens/enterprises/confirm_delete_enterprise_dialog.dart';
 import 'package:admin_app/widgets/address_list_tile.dart';
 import 'package:admin_app/widgets/animated_expanding_card.dart';
+import 'package:admin_app/widgets/email_list_tile.dart';
+import 'package:admin_app/widgets/phone_list_tile.dart';
 import 'package:common/models/enterprises/enterprise.dart';
+import 'package:common/models/generic/phone_number.dart';
 import 'package:common/utils.dart';
 import 'package:flutter/material.dart';
 
@@ -36,6 +39,11 @@ class EnterpriseListTileState extends State<EnterpriseListTile> {
   void dispose() {
     _nameController.dispose();
     _addressController.dispose();
+    _contactFirstNameController.dispose();
+    _contactLastNameController.dispose();
+    _contactFunctionController.dispose();
+    _contactPhoneController.dispose();
+    _contactEmailController.dispose();
     super.dispose();
   }
 
@@ -48,10 +56,35 @@ class EnterpriseListTileState extends State<EnterpriseListTile> {
   late final _addressController = AddressController(
     initialValue: widget.enterprise.address,
   );
+  late final _contactFirstNameController = TextEditingController(
+    text: widget.enterprise.contact.firstName,
+  );
+  late final _contactLastNameController = TextEditingController(
+    text: widget.enterprise.contact.lastName,
+  );
+  late final _contactFunctionController = TextEditingController(
+    text: widget.enterprise.contactFunction,
+  );
+  late final _contactPhoneController = TextEditingController(
+    text: widget.enterprise.contact.phone?.toString(),
+  );
+  late final _contactEmailController = TextEditingController(
+    text: widget.enterprise.contact.email,
+  );
 
   Enterprise get editedEnterprise => widget.enterprise.copyWith(
     name: _nameController.text,
     address: _addressController.address,
+    contact: widget.enterprise.contact.copyWith(
+      firstName: _contactFirstNameController.text,
+      lastName: _contactLastNameController.text,
+      phone: PhoneNumber.fromString(
+        _contactPhoneController.text,
+        id: widget.enterprise.contact.phone?.id,
+      ),
+      email: _contactEmailController.text,
+    ),
+    contactFunction: _contactFunctionController.text,
   );
 
   @override
@@ -134,7 +167,12 @@ class EnterpriseListTileState extends State<EnterpriseListTile> {
         padding: const EdgeInsets.only(left: 24.0, bottom: 8),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [_buildName(), _buildAddress()],
+          children: [
+            _buildName(),
+            _buildAddress(),
+            const SizedBox(height: 8),
+            _buildContact(),
+          ],
         ),
       ),
     );
@@ -168,11 +206,83 @@ class EnterpriseListTileState extends State<EnterpriseListTile> {
     return Padding(
       padding: const EdgeInsets.only(right: 12.0),
       child: AddressListTile(
-        title: 'Adresse de l\'entrerprise',
+        title: 'Adresse de l\'entreprise',
         addressController: _addressController,
         isMandatory: true,
         enabled: _isEditing,
       ),
+    );
+  }
+
+  Widget _buildContact() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _isEditing
+            ? Text('Contact')
+            : Text(
+              'Contact : ${widget.enterprise.contact.toString()} (${widget.enterprise.contactFunction})',
+            ),
+        Padding(
+          padding: const EdgeInsets.only(left: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (_isEditing)
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: _contactFirstNameController,
+                        decoration: const InputDecoration(labelText: 'Prénom'),
+                        validator: (value) {
+                          if (value?.isEmpty == true) {
+                            return 'Le prénom du contact est requis';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    Expanded(
+                      child: TextFormField(
+                        controller: _contactLastNameController,
+                        decoration: const InputDecoration(
+                          labelText: 'Nom de famille',
+                        ),
+                        validator: (value) {
+                          if (value?.isEmpty == true) {
+                            return 'Le nom du contact est requis';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              if (_isEditing)
+                TextFormField(
+                  controller: _contactFunctionController,
+                  decoration: const InputDecoration(
+                    labelText: 'Fonction dans l\'entreprise',
+                  ),
+                ),
+              const SizedBox(height: 4),
+              PhoneListTile(
+                controller: _contactPhoneController,
+                isMandatory: false,
+                enabled: _isEditing,
+              ),
+              const SizedBox(height: 4),
+              EmailListTile(
+                controller: _contactEmailController,
+                isMandatory: false,
+                enabled: _isEditing,
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
