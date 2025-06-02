20b570cf87985b800a586eef48856cc30ffd79e0
Pariterre <pariterre@hotmail.com>
2025-06-02 13:57:53 -0400

    All the admin is now independent of schoolboard

--- admin_app/lib/screens/enterprises/enterprises_list_screen.dart
+++ admin_app/lib/screens/enterprises/enterprises_list_screen.dart
@@ -1,8 +1,11 @@
 import 'package:admin_app/providers/enterprises_provider.dart';
+import 'package:admin_app/providers/school_boards_provider.dart';
 import 'package:admin_app/screens/drawer/main_drawer.dart';
 import 'package:admin_app/screens/enterprises/add_enterprise_dialog.dart';
 import 'package:admin_app/screens/enterprises/enterprise_list_tile.dart';
+import 'package:admin_app/widgets/animated_expanding_card.dart';
 import 'package:common/models/enterprises/enterprise.dart';
+import 'package:common/models/school_boards/school_board.dart';
 import 'package:flutter/material.dart';
 
 class EnterprisesListScreen extends StatelessWidget {
@@ -10,13 +13,25 @@ class EnterprisesListScreen extends StatelessWidget {
 
   static const route = '/enterprises_list';
 
-  Future<List<Enterprise>> _getEnterprises(BuildContext context) async {
-    final enterprises = [...EnterprisesProvider.of(context, listen: true)];
-    enterprises.sort((a, b) {
+  Future<Map<SchoolBoard, List<Enterprise>>> _getEnterprises(
+    BuildContext context,
+  ) async {
+    final schoolBoards = SchoolBoardsProvider.of(context, listen: true);
+
+    final allEnterprises = [...EnterprisesProvider.of(context, listen: true)];
+    allEnterprises.sort((a, b) {
       final nameA = a.name.toLowerCase();
       final nameB = b.name.toLowerCase();
       return nameA.compareTo(nameB);
     });
+
+    final enterprises = <SchoolBoard, List<Enterprise>>{};
+    for (final schoolBoard in schoolBoards) {
+      enterprises[schoolBoard] =
+          allEnterprises
+              .where((enterprise) => enterprise.schoolBoardId == schoolBoard.id)
+              .toList();
+    }
     return enterprises;
   }
 
@@ -49,21 +64,40 @@ class EnterprisesListScreen extends StatelessWidget {
         child: FutureBuilder(
           future: Future.wait([_getEnterprises(context)]),
           builder: (context, snapshot) {
-            final enterprises = snapshot.data?[0];
-            if (enterprises == null) {
+            final schoolBoards = snapshot.data?[0];
+            if (schoolBoards == null) {
               return const Center(child: CircularProgressIndicator());
             }
 
             return Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                if (enterprises.isEmpty)
+                if (schoolBoards.isEmpty)
                   const Center(child: Text('Aucune entreprise inscrite')),
-                if (enterprises.isNotEmpty)
-                  ...enterprises.map(
-                    (enterprise) => EnterpriseListTile(
-                      key: ValueKey(enterprise.id),
-                      enterprise: enterprise,
+                if (schoolBoards.isNotEmpty)
+                  ...schoolBoards.entries.map(
+                    (schoolBoardEntry) => Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: AnimatedExpandingCard(
+                        header: Text(
+                          schoolBoardEntry.key.name,
+                          style: Theme.of(
+                            context,
+                          ).textTheme.titleLarge!.copyWith(color: Colors.black),
+                        ),
+                        elevation: 0.0,
+                        initialExpandedState: true,
+                        child: Column(
+                          children: [
+                            ...schoolBoardEntry.value.map(
+                              (enterprise) => EnterpriseListTile(
+                                key: ValueKey(enterprise.id),
+                                enterprise: enterprise,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
               ],
