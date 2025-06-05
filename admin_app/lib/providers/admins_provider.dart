41db2bde286d28187201b770d5ad77961db5d74c
Pariterre <pariterre@hotmail.com>
2025-06-05 14:44:58 -0400

    Preventing non superAdmin to fetch admins data

--- admin_app/lib/providers/admins_provider.dart
+++ admin_app/lib/providers/admins_provider.dart
@@ -1,6 +1,7 @@
 import 'package:admin_app/providers/auth_provider.dart';
 import 'package:admin_app/providers/backend_list_provided.dart';
 import 'package:common/communication_protocol.dart';
+import 'package:common/models/generic/access_level.dart';
 import 'package:common/models/persons/admin.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -21,6 +22,12 @@ class AdminsProvider extends BackendListProvided<Admin> {
   }
 
   void initializeAuth(AuthProvider auth) {
+    if (!auth.isFullySignedIn ||
+        (auth.databaseAccessLevel ?? AccessLevel.teacher) <=
+            AccessLevel.superAdmin) {
+      return;
+    }
+
     initializeFetchingData(authProvider: auth);
     auth.addListener(() => initializeFetchingData(authProvider: auth));
   }
