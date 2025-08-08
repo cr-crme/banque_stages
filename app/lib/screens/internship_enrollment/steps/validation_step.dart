19e3a1b4b42762f6824b7d8fde68aef5f50fc644
Pariterre <pariterre@hotmail.com>
2025-08-08 13:04:08 -0400

    Continuing

--- app/lib/screens/internship_enrollment/steps/validation_step.dart
+++ app/lib/screens/internship_enrollment/steps/validation_step.dart
@@ -1,4 +1,5 @@
 import 'package:common/models/internships/internship.dart';
+import 'package:common_flutter/providers/enterprises_provider.dart';
 import 'package:common_flutter/providers/students_provider.dart';
 import 'package:crcrme_banque_stages/common/widgets/sub_title.dart';
 import 'package:flutter/material.dart';
@@ -22,11 +23,18 @@ class ValidationStep extends StatelessWidget {
       if (name != null) studentName = name;
     }
 
+    final enterprise =
+        EnterprisesProvider.of(context).fromIdOrNull(internship?.enterpriseId);
+    internship?.jobId;
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const SubTitle('Stagiaire', left: 0),
         Text(studentName),
+        const SizedBox(height: 16),
+        const SubTitle('Métier(s)', left: 0),
+        intern
       ],
     );
   }
